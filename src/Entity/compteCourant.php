6a673813590ac577d56a4abc221fa453b08da578
Yassine Sekkah <yassinesekkah11@gmail.com>
2025-12-25 14:09:16 +0100

    Feat: ABCP-46 Créer la classe CompteEpargne

--- src/Entity/compteCourant.php
+++ src/Entity/compteCourant.php
@@ -1,36 +1,38 @@
-<?php 
+<?php
 
-class compteCourant extends Compte 
+class compteCourant extends Compte
 {
     private float $fees = 5;
 
     public function deposit(float $amount): void
     {
-        if($amount <= 0){
-            throw new InvalidArgumentException("Le montant doit être positif");
+        if (!is_numeric($amount) || $amount <= 0) {
+            throw new InvalidArgumentException("Le montant doit être positif et numérique");
         }
 
-        $soldeAvant = $this -> getSolde();
-        $soldeApres = $soldeAvant + $amount - $this -> fees;
 
-        $this -> setSolde($soldeApres);
+        $soldeAvant = $this->getSolde();
+        $soldeApres = $soldeAvant + $amount - $this->fees;
+
+        $this->setSolde($soldeApres);
         echo "Dépôt : $amount DH, Frais : 5 DH, Nouveau solde: $soldeApres DH <br>";
     }
 
     public function withdraw(float $amount): void
     {
-        if($amount <= 0){
-            throw new InvalidArgumentException("Le montant doit être positif");
+        if (!is_numeric($amount) || $amount <= 0) {
+            throw new InvalidArgumentException("Le montant doit être positif et numérique");
         }
-        $soldeAvant = $this -> getSolde();
+
+        $soldeAvant = $this->getSolde();
         $maxWithraw = $soldeAvant + 5000;
-        
-        if($amount > $maxWithraw){
-            throw new InvalidArgumentException("le montant demandé dépasse le solde autorisé du compte");
+
+        if ($amount > $maxWithraw) {
+            throw new InvalidArgumentException("Impossible de retirer $amount DH, solde disponible : $soldeAvant DH");
         }
         $soldeApres = $soldeAvant - $amount;
 
-        $this -> setSolde($soldeApres);
+        $this->setSolde($soldeApres);
 
         echo "Retrait : $amount DH, Nouveau solde : $soldeApres DH <br>";
     }
---
+++ src/Entity/compteEpargne.php
@@ -0,0 +1,35 @@
+<?php
+
+class CompteEpargne extends Compte
+{
+    public function deposit(float $amount): void
+    {
+        if (!is_numeric($amount) || $amount <= 0) {
+            throw new InvalidArgumentException("Le montant doit être positif et numérique");
+        }
+        $soldeAvant = $this->getSolde();
+
+        $soldeApres = $soldeAvant + $amount;
+
+        $this->setSolde($soldeApres);
+
+        echo "Dépôt : $amount DH, Nouveau solde: $soldeApres DH <br>";
+    }
+
+    public function withdraw(float $amount): void
+    {
+        if (!is_numeric($amount) || $amount <= 0) {
+            throw new InvalidArgumentException("Le montant doit être positif et numérique");
+        }
+        //hna makayench l9adia dyal ti7 3la 0 donc khdam wsafi 
+        $soldeAvant = $this -> getSolde();
+
+        if($amount > $soldeAvant){
+            throw new InvalidArgumentException("Impossible de retirer $amount DH, solde disponible : $soldeAvant DH");
+        }
+        $soldeApres = $soldeAvant - $amount;
+
+        $this -> setSolde($soldeApres);
+         echo "Retrait : $amount DH, Nouveau solde : $soldeApres DH <br>";
+    }
+}
