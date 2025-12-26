5f346dc09f2aefe138e43f539bdb1188bee58e30
Yassine Sekkah <yassinesekkah11@gmail.com>
2025-12-26 12:23:17 +0100

    Feat: Ajouter clientHasCourant pour vérifier l’existence d’un compte courant

--- src/Repository/CompteRepository.php
+++ src/Repository/CompteRepository.php
@@ -23,13 +23,11 @@ class CompteRepository
 
     public function createAccount(int $clientId, string $accountType): Compte
     {
-        // التأكد أن العميل موجود
         $client = $this->clientRepo->findById($clientId);
         if (!$client) {
             throw new Exception("Il n'existe pas de client avec cet id");
         }
 
-        // التأكد من نوع الحساب
         if ($accountType !== 'courant' && $accountType !== 'epargne') {
             throw new InvalidArgumentException(
                 "Type de compte invalide, entrez 'courant' ou 'epargne'"
@@ -39,7 +37,6 @@ class CompteRepository
            throw new Exception("Ce client possède déjà un compte courant");
         }
 
-        // إدخال الحساب في DB
         $stmt = $this->pdo->prepare(
             "INSERT INTO compte (solde, type, client_id) VALUES (:solde, :type, :client_id)"
         );
@@ -49,50 +46,14 @@ class CompteRepository
             'client_id' => $clientId
         ]);
 
-        // id الجديد من DB
         $accountId = (int) $this->pdo->lastInsertId();
 
-        // إنشاء object المناسب مع id و return
         if ($accountType === 'courant') {
             return new CompteCourant($accountId, 0, $clientId);
         } else {
             return new CompteEpargne($accountId, 0, $clientId);
         }
     }
-
-    // public function createAccount(int $clientId,string $accountType): Compte
-    // {
-    //     $client = $this->clientRepo->findById($clientId);
-
-    //     if (!$client) {
-    //         throw new Exception("Il n'existe pas de client avec cet id");
-    //     }
-
-    //     if ($accountType !== 'courant' && $accountType !== 'epargne') {
-    //         throw new InvalidArgumentException("Invalid type de compte, 
-    //                                                 s'il vous plait entrait 'courant' ou 'epargne'");
-    //     }
-    //     if($accountType == 'courant' && $this -> clientHasCourant($clientId)){
-    //         throw new Exception("Ce client possède déjà un compte courant");
-    //     }
-
-    //     $stmt = $this->pdo->prepare
-    //                     ("INSERT INTO compte (solde, type, client_id) VALUES (:solde, :type, :client_id)");
-    //     $stmt->execute([
-    //         'solde' => 0,
-    //         'type' => $accountType,
-    //         'client_id' => $clientId
-    //     ]);
-
-    //     $accountId = (int) $this -> pdo -> lastInsertId();
-    //     //return 3la hsab type dyal compte 
-    //     if($accountType === 'courant'){
-    //         return new CompteCourant($accountId, 0, $clientId);
-    //     }
-    //     else{
-    //         return new CompteEpargne($accountId, 0, $clientId);
-    //     }
-    // }
 
     public function updateSolde(Compte $compte)
     {
