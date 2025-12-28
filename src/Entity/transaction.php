5f151c8c2594d215f9dec7204e13caf41fdb67bb
Yassine Sekkah <yassinesekkah11@gmail.com>
2025-12-28 18:41:25 +0100

    Feat: ABCP-60 Créer la classe Transaction

---
+++ src/Entity/transaction.php
@@ -0,0 +1,50 @@
+<?php 
+
+class Transaction 
+{
+    private ?int $id;
+    private int $compteId;
+    private string $type;
+    private float $amount;
+    private DateTimeImmutable $creatAt;
+
+    public function __construct(int $compteId, string $type, float $amount)
+    {
+        if($amount <= 0){
+            throw new InvalidArgumentException("Montant invalide");
+        }
+        $this -> compteId = $compteId;
+        $this -> type = $type;
+        $this -> amount = $amount;
+        $this -> creatAt = new DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this -> id;
+    }
+    /// nehtajoha f repository mn ba3d mansayviw f db 
+    public function setId($id): void
+    {
+        $this -> id = $id;
+    }
+
+    public function getCompteId(): int
+    {
+        return $this -> compteId;
+    }
+
+    public function getType(): string
+    {
+        return $this -> type;
+    }
+
+    public function getAmount(): float
+    {
+        return $this -> amount;
+    }
+    public function getCreatAt(): DateTimeImmutable
+    {
+        return $this -> creatAt;
+    }
+}
